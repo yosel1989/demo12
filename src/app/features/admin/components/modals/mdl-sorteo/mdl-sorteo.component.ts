4d8f5439b302628a4ce7f80ed753c9adcd0f65a8
YoselAguirreBalbin <sistemas4@carolina-peru.pe>
2025-09-13 13:14:09 -0500

    .

--- src/app/features/admin/components/modals/mdl-sorteo/mdl-sorteo.component.ts
+++ src/app/features/admin/components/modals/mdl-sorteo/mdl-sorteo.component.ts
@@ -2,10 +2,12 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { DatePicker } from 'primeng/datepicker';
 import { FormsModule } from '@angular/forms';
 import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
+import { TextareaModule } from 'primeng/textarea';
 
 @Component({
   selector: 'app-mdl-sorteo',
-  imports: [FormsModule, DatePicker, InputNumberModule],
+  imports: [FormsModule, DatePicker, InputNumberModule, InputTextModule, TextareaModule  ],
   templateUrl: './mdl-sorteo.component.html',
   styleUrl: './mdl-sorteo.component.scss'
 })
